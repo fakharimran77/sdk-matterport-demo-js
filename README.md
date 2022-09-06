49fdbb14fc3510b646f996ff78e580b7ca2ab29d
Antonio Hofmeister Ribeiro <hof.antonio@gmail.com>
2022-09-06 18:16:36 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# SuperViz + Matterport Demo in Javascript
+
+<p align="center">
+   <a href="https://superviz.com/" target="blank"><img src="https://avatars.githubusercontent.com/u/56120553?s=200&v=4" width="120" alt="SuperViz Logo" /></a>
+</p>
+
+## Quick start
+
+Clone the project repository:
+
+```bash
+git@github.com:SuperViz/sdk-matterport-demo-js.git
+```
+
+Fill the constant ```<SUPERVIZ_DEVELOPER_TOKEN>``` in ```modules/superviz/supervizSdk.js``` with your superviz developer token.
+
+Fill the constant ```<MATTERPORT_DEVELOPER_KEY>``` in ```modules/matterport/matterportSdk.js``` with your matterport developer credentials.
+
+
+### From the project root, run a local web server and access using localhost.
---
+++ index.js
@@ -0,0 +1,34 @@
+import { supervizSdk } from "./modules/superviz/supervizSdk.js"
+import { changeContent } from './modules/matterport/matterportSdk.js'
+
+const CONTENT_SYNC_PROPERTY = "content";
+
+const MATTERPORT_MODELS = [ // example models
+    "pCtbBSaDwgQ",
+    "NARFMvjQ3wq",
+    "piqHj4JP6Fp"
+];
+
+let currentContentIndex = 0;
+
+changeContent(MATTERPORT_MODELS[0]);
+
+supervizSdk.subscribe(CONTENT_SYNC_PROPERTY, function (newModelSid) {
+    changeContent(newModelSid)
+});
+
+const sendSyncCommand = function (index) {
+    supervizSdk.setSyncProperty({ [CONTENT_SYNC_PROPERTY]: MATTERPORT_MODELS[index] });
+}
+
+document.getElementById('previousButton').onclick = function () {
+    currentContentIndex--;
+    const index = Math.abs(currentContentIndex % MATTERPORT_MODELS.length);
+    sendSyncCommand(index)
+};
+
+document.getElementById('nextButton').onclick = function () {
+    currentContentIndex++;
+    const index = Math.abs(currentContentIndex % MATTERPORT_MODELS.length);
+    sendSyncCommand(index)
+};
