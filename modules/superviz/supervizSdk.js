6822919ace91cd3ec11e398a5c421ffc1f305ba1
Antonio Hofmeister Ribeiro <hof.antonio@gmail.com>
2023-01-13 14:50:34 +0100

    feat: update sdk versions and add change content with adapter

--- modules/superviz/supervizSdk.js
+++ modules/superviz/supervizSdk.js
@@ -1,11 +1,11 @@
-const DEVELOPER_KEY = "<SUPERVIZ_DEVELOPER_TOKEN>";
+const DEVELOPER_KEY = "<SUPERVIZ_DEVELOPER_KEY>";
 export const supervizSdk = await SuperVizSdk.init(DEVELOPER_KEY, {
     userGroup: {
         id: "<GROUP-ID>",
         name: "<GROUP-NAME>"
     },
     user: {
-        id: "<MY-USER-ID>",
+        id: "<MY-USER-ID>", // make sure you don't use the same user id for different instances
         name: "<MY-USER-NAME>",
         isHostCandidate: true,
     },
