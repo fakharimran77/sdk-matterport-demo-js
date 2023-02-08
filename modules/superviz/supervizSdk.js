2615f94b911257a4d824101e4980d92629fc593a
Vinicius <afonsovinicius@yahoo.com.br>
2023-02-08 09:20:58 -0300

    chore: adjust texts

--- modules/superviz/supervizSdk.js
+++ modules/superviz/supervizSdk.js
@@ -1,4 +1,4 @@
-const DEVELOPER_KEY = "<SUPERVIZ_DEVELOPER_KEY>";
+const DEVELOPER_KEY = "i7w15mfdvlq611xunai5m3gzxwvg2x";
 
 const url = new URL(document.URL);
 let userName = url.searchParams.get('user-name');
