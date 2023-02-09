2848ab64e7a90550d11e3c363777294a6318c1bd
Carlos <carlos.santos74@outlook.com>
2023-02-09 07:45:34 -0300

    fix: current project sync on guest joined

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const CONTENT_SYNC_PROPERTY = "content";
 let changedContent = false;
 let matterportPluginInstance = null;
 
-// load first model
-changeContent("v4LWLiLDm3s");
+let currentContent;
+
+let myParticipant;
+let participantList = []
 
 function loadPlugin() {
     const plugin = new window.MatterportPlugin(matterportSdk);
@@ -20,11 +22,13 @@ function loadPlugin() {
 }
 
 // joined meeting
-supervizSdk.subscribe(SuperVizSdk.MeetingEvent.MY_PARTICIPANT_JOINED, () => {
+supervizSdk.subscribe(SuperVizSdk.MeetingEvent.MY_PARTICIPANT_JOINED, (participant) => {
+    myParticipant = participant;
+
     document.getElementById("wrapper").style.display = "none";
     // interface buttons to change content
     document.getElementById('v4LWLiLDm3s').onclick = function () {
-        sendSyncCommand('v4LWLiLDm3s')
+        syncContent('v4LWLiLDm3s')
         document.getElementsByClassName('project-name')[0].classList.remove('active')
         document.getElementsByClassName('project-name')[1].classList.remove('active')
         document.getElementsByClassName('project-name')[2].classList.remove('active')
@@ -32,7 +36,7 @@ supervizSdk.subscribe(SuperVizSdk.MeetingEvent.MY_PARTICIPANT_JOINED, () => {
         document.getElementById('v4LWLiLDm3s').getElementsByClassName('project-name')[0].classList.add('active')
     };
     document.getElementById('zWpt6DAXDJw').onclick = function () {
-        sendSyncCommand('zWpt6DAXDJw')
+        syncContent('zWpt6DAXDJw')
         document.getElementsByClassName('project-name')[0].classList.remove('active')
         document.getElementsByClassName('project-name')[1].classList.remove('active')
         document.getElementsByClassName('project-name')[2].classList.remove('active')
@@ -40,7 +44,7 @@ supervizSdk.subscribe(SuperVizSdk.MeetingEvent.MY_PARTICIPANT_JOINED, () => {
         document.getElementById('zWpt6DAXDJw').getElementsByClassName('project-name')[0].classList.add('active')
     };
     document.getElementById('KSParQVSEKy').onclick = function () {
-        sendSyncCommand('KSParQVSEKy')
+        syncContent('KSParQVSEKy')
         document.getElementsByClassName('project-name')[0].classList.remove('active')
         document.getElementsByClassName('project-name')[1].classList.remove('active')
         document.getElementsByClassName('project-name')[2].classList.remove('active')
@@ -48,15 +52,29 @@ supervizSdk.subscribe(SuperVizSdk.MeetingEvent.MY_PARTICIPANT_JOINED, () => {
         document.getElementById('KSParQVSEKy').getElementsByClassName('project-name')[0].classList.add('active')
     };
     document.getElementById('toTRYzoAMdT').onclick = function () {
-        sendSyncCommand('toTRYzoAMdT')
+        syncContent('toTRYzoAMdT')
         document.getElementsByClassName('project-name')[0].classList.remove('active')
         document.getElementsByClassName('project-name')[1].classList.remove('active')
         document.getElementsByClassName('project-name')[2].classList.remove('active')
         document.getElementsByClassName('project-name')[3].classList.remove('active')
         document.getElementById('toTRYzoAMdT').getElementsByClassName('project-name')[0].classList.add('active')
     };
-    loadPlugin();
 });
+
+supervizSdk.subscribe(SuperVizSdk.MeetingEvent.MEETING_PARTICIPANT_LIST_UPDATE, (participants) => {
+    const isHost = participants.find(participant => participant.id === myParticipant.id).isHost;
+    
+    if(participants.length === 1 && !participantList.length) { 
+      syncContent('v4LWLiLDm3s')
+    }
+
+    if(participantList.length < participants.length && isHost)  {
+      syncContent(currentContent)
+    }
+
+    participantList = participants;
+})
+
 // interface buttons to open close
 document.getElementById('collapsedButton').onclick = function () {
     if (document.getElementById('sectionReady').classList.contains('hidden')) {
@@ -65,23 +83,26 @@ document.getElementById('collapsedButton').onclick = function () {
     document.getElementById('sectionReady').classList.add('hidden')
 };
 // send change content sync
-const sendSyncCommand = function (index) {
-    supervizSdk.setSyncProperty(CONTENT_SYNC_PROPERTY, index);
+const syncContent = function (newModelSid) {
+    supervizSdk.setSyncProperty(CONTENT_SYNC_PROPERTY, newModelSid);
 }
 
 // receive change content sync
 supervizSdk.subscribe(CONTENT_SYNC_PROPERTY, function (newModelSid) {
+    if(newModelSid === currentContent) return;
+    
     if (matterportPluginInstance) { // disconnect from plugin if there is one
         supervizSdk.unloadPlugin();
         matterportPluginInstance = null;
         changedContent = true;
     }
-    changeContent(newModelSid) // send change content to matterport // see modules/matterport/matterportSdk.js
+
+    
+    changeContent(newModelSid); // send change content to matterport // see modules/matterport/matterportSdk.js
+    currentContent = newModelSid;
 });
 
 // received matterport loaded new content
 export const onContentChanged = () => {
-    if (changedContent) {
-        loadPlugin(); // reconnect plugin
-    }
+    loadPlugin(); // reconnect plugin
 }
